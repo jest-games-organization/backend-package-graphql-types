195361cdd938dbe3c4685d10e4f96e63446e5cac
rconrardy <conrardy@umich.edu>
2022-10-19 05:47:31 -0400

    Fix ConnectionArgs type.

--- src/types/ConnectionArgs.ts
+++ src/types/ConnectionArgs.ts
@@ -5,5 +5,5 @@ export type ConnectionArgs<Record extends { [key: string]: unknown }> = {
   first?: number | null | undefined;
   after?: string | null | undefined;
   filterBy?: FilterBy<Record> | null | undefined;
-  orderBy?: OrderBy<Record> | null | undefined;
+  orderBy?: OrderBy<Record>[] | null | undefined;
 };
