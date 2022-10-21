2f9f69978a80b8a0d9969650ecd0428a5915f418
rconrardy <conrardy@umich.edu>
2022-10-21 17:54:35 -0400

    Make OrderBy type not array.

--- src/types/DecodedCursor.ts
+++ src/types/DecodedCursor.ts
@@ -2,5 +2,5 @@ import { OrderBy } from './OrderBy';
 
 export type DecodedCursor<Record extends { [key: string]: unknown }> = {
   data: { [key in keyof Record]: Record[key] };
-  args: { orderBy: OrderBy<Record> };
+  args: { orderBy: OrderBy<Record>[] };
 };
