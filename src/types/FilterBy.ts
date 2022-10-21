1fc8272ea245b1f5bc109d48c6470f4514548955
rconrardy <conrardy@umich.edu>
2022-10-21 17:42:18 -0400

    Fix FilterBy type.

--- src/types/FilterBy.ts
+++ src/types/FilterBy.ts
@@ -1,7 +1,7 @@
 export type FilterBy<Record extends { [key: string]: unknown }> = {
-  [key in keyof Record]?: unknown;
+  [key in keyof Record]?: any;
 } & {
-  AND?: FilterBy<Record>[];
-  OR?: FilterBy<Record>[];
-  NOT?: FilterBy<Record>[];
+  AND?: FilterBy<Record>[] | null | undefined;
+  OR?: FilterBy<Record>[] | null | undefined;
+  NOT?: FilterBy<Record>[] | null | undefined;
 };
