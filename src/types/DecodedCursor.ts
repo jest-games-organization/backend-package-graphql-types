d9c23000c6856d101225d43998e2c8648be66394
rconrardy <conrardy@umich.edu>
2022-11-10 18:09:32 -0500

    Update DecodedCursor type.

--- src/types/DecodedCursor.ts
+++ src/types/DecodedCursor.ts
@@ -1,7 +1,6 @@
 import { DataObject } from '@jest-games-organization/backend-package-object-types';
-import { OrderByInput } from './OrderByInput';
 
 export type DecodedCursor<Record extends DataObject> = {
   data: { [key in keyof Record]: Record[key] };
-  args: { orderBy: OrderByInput<Record>[] };
+  args: { orderBy: { [key in keyof Record]?: 'asc' | 'desc' }[] };
 };
